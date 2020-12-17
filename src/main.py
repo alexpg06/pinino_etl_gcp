4f9785ec40d92465ae225e8d07ac8a3cca5027ab
alexis padilla <alexpg06@gmail.com>
2020-12-16 21:27:29 -0500

    First commit
    Adding subfolders and files

---
+++ src/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+from http import HTTPStatus
+# import requests
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # Validacion de libreria HTTP
+    print(f'{HTTPStatus.OK}')
+
+    # Homework
+    # crear un repositorio en Git, clonar y crear el proyecto Pinino_etl_gcp
+    # Crear el sistema que se conecte al API de metrobus CDMX (request)
+    # Looking for Pandas (how it works)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
